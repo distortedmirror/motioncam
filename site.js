734300635752de3823f73de01d25edd75493ffbe
Mark Brito <mark.brito.gcloud@gmail.com>
2020-01-12 02:24:17 -0800

    ok

--- site.js
+++ site.js
@@ -1,6 +1,7 @@
 var video = document.getElementById('video');
 var canvas = document.getElementById('motion');
 var score = document.getElementById('score');
+var videoImage=document.getElementById('videoImage');
 var capCount=0;
 
 function initSuccess() {
@@ -14,14 +15,17 @@ function initError() {
 function capture(payload) {
 	var rangeCap=document.getElementById('rangeCapture');
 	var divCapture = document.getElementById('divCapture');
+	var numThreshold= document.getElementById('numberThreshold');
+
 	var sc = payload.score;
 	score.textContent = payload.score;
-	if(sc>=30){
-        var img = document.createElement("img");
-        img.src = payload.getURL();
-	img.setAttribute('style','margin:2px;position:relative;height:100%;width:auto;');
-        divCapture.append(img);
-	capCount++;
+	if(sc>=numThreshold){
+	        var img = document.createElement("img");
+       		img.src = payload.getURL();
+		img.setAttribute('style','margin:2px;position:relative;height:100%;width:auto;');
+		img.setAttribute('onclick','videoImage.src=this.src;videoImage.style.display="block";');
+		divCapture.append(img);
+		capCount++;
 		rangeCap.setAttribute('max',capCount);
 		rangeCap.setAttribute('value',capCount);
 	}
