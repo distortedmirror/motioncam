443082789dec0b79a6696a5169541de7cff062ec
Mark Brito <mark.brito.gcloud@gmail.com>
2020-01-12 02:43:39 -0800

    ok

--- site.js
+++ site.js
@@ -2,6 +2,9 @@ var video = document.getElementById('video');
 var canvas = document.getElementById('motion');
 var score = document.getElementById('score');
 var videoImage=document.getElementById('videoImage');
+var rangeCap=document.getElementById('rangeCapture');
+var divCapture = document.getElementById('divCapture');
+var numThreshold= document.getElementById('numberThreshold');
 var capCount=0;
 
 function initSuccess() {
@@ -13,17 +16,15 @@ function initError() {
 }
 
 function capture(payload) {
-	var rangeCap=document.getElementById('rangeCapture');
-	var divCapture = document.getElementById('divCapture');
-	var numThreshold= document.getElementById('numberThreshold');
 
 	var sc = payload.score;
 	score.textContent = payload.score;
 	if(sc>=numThreshold.value){
 	        var img = document.createElement("img");
        		img.src = payload.getURL();
-		img.setAttribute('style','margin:2px;position:relative;height:100%;width:auto;');
-		img.setAttribute('onclick','videoImage.src=this.src;videoImage.style.display="block";');
+		img.setAttribute('style','margin:2px;position:relative;height:45%;width:auto;');
+		rangeCap.setAttribute('onchange','videoImage.src=divCapture.getElementsByTagId("img")[this.value].src;videoImage.style.display="block";setTimeout(function(){videoImage.style.display="none";},3000);');
+		img.setAttribute('onclick','videoImage.src=this.src;videoImage.style.display="block";setTimeout(function(){videoImage.style.display="none";},3000);');
 		divCapture.append(img);
 		capCount++;
 		rangeCap.setAttribute('max',capCount);
