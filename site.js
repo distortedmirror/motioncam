b94d0f97380b34d46ea650bc2d4afddd13f7091e
Mark Brito <mark.brito.gcloud@gmail.com>
2020-03-02 04:07:33 -0600

    ok

--- site.js
+++ site.js
@@ -17,14 +17,49 @@ function initError() {
 	debugger;
 	alert('Something went wrong.');
 }
+function thumbnail(base64, maxWidth, maxHeight) {
 
+  // Max size for thumbnail
+  if(typeof(maxWidth) === 'undefined') var maxWidth = 500;
+  if(typeof(maxHeight) === 'undefined') var maxHeight = 500;
+
+  // Create and initialize two canvas
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  var canvasCopy = document.createElement("canvas");
+  var copyContext = canvasCopy.getContext("2d");
+
+  // Create original image
+  var img = new Image();
+  img.src = base64;
+
+  // Determine new ratio based on max size
+  var ratio = 1;
+  if(img.width > maxWidth)
+    ratio = maxWidth / img.width;
+  else if(img.height > maxHeight)
+    ratio = maxHeight / img.height;
+
+  // Draw original image in second canvas
+  canvasCopy.width = img.width;
+  canvasCopy.height = img.height;
+  copyContext.drawImage(img, 0, 0);
+
+  // Copy and resize second canvas to first canvas
+  canvas.width = img.width * ratio;
+  canvas.height = img.height * ratio;
+  ctx.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvas.width, canvas.height);
+
+  return canvas.toDataURL();
+
+}
 function capture(payload) {
 
 	var sc = payload.score;
 	score.textContent = payload.score;
 	if(sc>=numThreshold.value){
 	        var img = document.createElement("img");
-       		img.src = payload.getURL();
+       		img.src =thumbnail( payload.getURL(),86,72);
 		function formatAMPM(date) { // This is to display 12 hour format like you asked
 		  var hours = date.getHours();
 		  var minutes = date.getMinutes();
@@ -60,5 +95,4 @@ DiffCamEngine.init({
 	motionCanvas: canvas,
 	initSuccessCallback: initSuccess,
 	initErrorCallback: initError,
-	captureCallback: capture
-});
+	captureCallback: 
