390032a4dac1a9cb755022c3d04766838d59ae4b
Mark Brito <mark.brito.gcloud@gmail.com>
2020-05-25 07:56:58 -0600

    integration scripts

--- test.pl
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/perl -w
-print "Content-type: text/html\r\n\r\n";
-print "CGI working perfectly!! \n";
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-perl -e 'print "Content-type: text/html\r\n\r\n";'
-echo ok bash
