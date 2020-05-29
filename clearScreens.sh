4ac10b72cb533547fd4ae62174cf4f52cbc5b86a
Mark Brito <mark.brito.gcloud@gmail.com>
2020-05-29 10:39:08 -0600

    Mutex

--- clearScreens.sh
+++ clearScreens.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+((flock --timeout 60 8 || exit 1 ) &&  (
 cd /usr/lib/cgi-bin/ 2> /dev/null
 rm -f ./screens/* 2> /dev/null
 rm -f ./screens.html 2> /dev/null
 perl -e 'print "Content-type: text/html\r\n";'
 #perl -e 'print "Content-Length: 0\r\n\r\n";'
+ ) ) 8> /var/lock/lock8
--- screen.sh
+++ screen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+((flock --timeout 60 8 || exit 1 ) &&  (
 cd /usr/lib/cgi-bin 2> /dev/null
 perl -e 'print "Content-type: text/html\r\n";'
 perl -e 'print "Content-Length: '`cat screen.html |wc -c`'\r\n\r\n";'
 cat screen.html
+ ) ) 8> /var/lock/lock8
