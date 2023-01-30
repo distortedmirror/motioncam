6738d93a522057de9cfca1413f10791123950ebf
distortedmirror <58996281+distortedmirror@users.noreply.github.com>
2023-01-29 23:33:31 -0800

    Change to /var/www/html/

--- camera.sh
+++ camera.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ((flock --timeout 60 8 || exit 1 ) &&  (
-cd /usr/lib/cgi-bin 2> /dev/null
+cd /var/www/html/ 2> /dev/null
 perl -e 'print "Content-type: text/html\r\n";'
 perl -e 'print "Content-Length: '`cat index.html|base64|wc -c`'\r\n\r\n";'
 cat index.html
