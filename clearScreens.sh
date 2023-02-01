3f91db8a648c09675839a6ce439876cf6ed05177
Mark Brito <mark.brito@gmail.com>
2023-01-31 17:03:11 -0800

    archive screens and caps

--- clearScreens.sh
+++ clearScreens.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ((flock --timeout 60 8 || exit 1 ) &&  (
 cd /var/www/html/ 2> /dev/null
-rm -f ./screens/* 2> /dev/null
-rm -f ./screens.html 2> /dev/null
+mv ./screens/* ./screensarchive/ 2> /dev/null
+mv ./screens.html screens-`date | sed 's/[ :]/-/g'`.html 2> /dev/null
 perl -e 'print "Content-type: text/html\r\n";'
 #perl -e 'print "Content-Length: 0\r\n\r\n";'
  ) ) 8> /var/lock/lock8
